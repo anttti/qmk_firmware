93c14d47b1430ba9d2e392f147fb7598a3546321
Antti Mattila <me@rarelyneeded.com>
2021-01-17 20:13:54 +0200

    Add a home row mod version of my layout

--- keyboards/hotdox/keymaps/anttti-home-row-mods/keymap.c
+++ keyboards/hotdox/keymaps/anttti-home-row-mods/keymap.c
@@ -5,6 +5,18 @@
 #define NAVI 1
 #define SYMB 2
 
+// Left-hand home row mods
+#define CTL_A LCTL_T(KC_A)
+#define ALT_S LALT_T(KC_S)
+#define GUI_D LGUI_T(KC_D)
+#define SFT_F LSFT_T(KC_F)
+
+// Right-hand home row mods
+#define SFT_J RSFT_T(KC_J)
+#define GUI_K RGUI_T(KC_K)
+#define ALT_L LALT_T(KC_L)
+#define CTL_SCLN RCTL_T(KC_SCLN)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Alphas
  *
@@ -13,37 +25,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | MehTab |   Q  |   W  |   E  |   R  |   T  |  [   |           |  ]   |   Y  |   U  |   I  |   O  |   P  |   '    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |Esc/Ctrl|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | Ent/Cmd|
+ * |Esc/Ctrl|Ctrl/A| Alt/S| Cmd/D|Shft/F|   G  |------|           |------|   H  |Shft/J| Cmd/D| Alt/L|Ctrl/;| Enter  |
  * |--------+------+------+------+------+------|  -   |           |  =   |------+------+------+------+------+--------|
  * | (/Shift|   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | )/Shift|
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | LGui |   \  | NOOP | NOOP | Alt  |                                       | LT2/`| NOOP | NOOP | NOOP | NOOP |
+ *   | LGui |   \  | NOOP | NOOP | Alt/Esc  |                                       | LT2/`| NOOP | NOOP | NOOP | NOOP |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | VolDn| VolUp|       | Play |ScrnSh|
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | PgUp |       | PgDn |        |      |
- *                                 | Mo(1)|Shift/|------|       |------|  Cmd/  |Space |
- *                                 |      | Tab  | Home |       | End  |  Enter |      |
- *                                 `--------------------'       `----------------------'
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      | PgUp |       | PgDn | Cmd/ |      |
+ *                                 | Mo(1)| Tab  |------|       |------| Enter|Space |
+ *                                 |      |      | Home |       | End  |      |      |
+ *                                 `--------------------'       `--------------------'
  */
 [BASE] = LAYOUT_ergodox(
   // left hand
   KC_NO,          KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_NO,
   MEH_T(KC_TAB),  KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRC,
-  LCTL_T(KC_ESC), KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+  LCTL_T(KC_ESC), CTL_A,        ALT_S,  GUI_D,  SFT_F,  KC_G,
   KC_LSPO,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_MINS,
-  KC_LGUI,        KC_BSLS,      KC_NO,  KC_NO,  KC_LALT,
+  KC_LGUI,        KC_BSLS,      KC_NO,  KC_NO,  LALT_T(KC_ESC),
 
-                                               KC__VOLDOWN,         KC__VOLUP,
-                                                                    KC_PGUP,
-                                        MO(1), MT(MOD_LSFT, KC_TAB),KC_HOME,
+                                               KC__VOLDOWN,   KC__VOLUP,
+                                                              KC_PGUP,
+                                        MO(1), MEH_T(KC_TAB), KC_HOME,
   // right hand
-  KC_NO,       KC_6,   KC_7,          KC_8,   KC_9,   KC_0,    KC_NO,
-  KC_RBRC,     KC_Y,   KC_U,          KC_I,   KC_O,   KC_P,    KC_QUOT,
-               KC_H,   KC_J,          KC_K,   KC_L,   KC_SCLN, GUI_T(KC_ENT),
-  KC_EQL,      KC_N,   KC_M,          KC_COMM,KC_DOT, KC_SLSH, KC_RSPC,
-                       LT(2, KC_GRV), KC_NO,  KC_NO,  KC_NO,   KC_NO,
+  KC_NO,       KC_6,   KC_7,          KC_8,   KC_9,   KC_0,     KC_NO,
+  KC_RBRC,     KC_Y,   KC_U,          KC_I,   KC_O,   KC_P,     KC_QUOT,
+               KC_H,   SFT_J,         GUI_K,  ALT_L,  CTL_SCLN, KC_ENT,
+  KC_EQL,      KC_N,   KC_M,          KC_COMM,KC_DOT, KC_SLSH,  KC_RSPC,
+                       LT(2, KC_GRV), KC_NO,  KC_NO,  KC_NO,    KC_NO,
 
   KC_MPLY, G(S(C(KC_4))),
   KC_PGDN,
@@ -57,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
  * |         |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |   F12  |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |         |      |      |      |      |      |------|           |------| Left | Down |  Up  | Right|      |        |
+ * |         | Ctrl | Alt  | Cmd  | Shift|      |------|           |------| Left | Down |  Up  | Right|      |        |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |         |      |      |      |      |      |      |           |      | Home | PgDn | PgUp | End  |      |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -67,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
+ *                                 |      |      |------|       |------|  Del | Bcks |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
@@ -75,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // left hand
   RESET,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
   KC_TRNS,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_TRNS,
-  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+  KC_TRNS,KC_LCTL,KC_LALT,KC_LGUI,KC_LSFT,KC_TRNS,
   KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
   KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                                   KC_TRNS,KC_TRNS,
@@ -89,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS,
   KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS
+  KC_TRNS, KC_DEL, KC_BSPC
 ),
 
 /* Keymap 2: Symbols
@@ -132,6 +144,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS,
   KC_TRNS, KC_TRNS, KC_WBAK
 )};
+
+// bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
+//   switch (keycode) {
+//     case KC_BSPC:
+//       return false;
+//     default:
+//       return true;
+//   }
+// }
 
 // bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 //   switch (keycode) {
