d2f1b6d04041f57c1138ee11b8f6a2b9cdabcd24
Antti Mattila <me@rarelyneeded.com>
2021-01-24 16:02:30 +0200

    Add Colemak layer

--- keyboards/hotdox/keymaps/anttti-home-row-mods/keymap.c
+++ keyboards/hotdox/keymaps/anttti-home-row-mods/keymap.c
@@ -4,6 +4,7 @@
 #define BASE 0
 #define NAVI 1
 #define SYMB 2
+#define COLEMAK 3
 
 // Left-hand home row mods
 #define CTL_A LCTL_T(KC_A)
@@ -17,11 +18,22 @@
 #define ALT_L LALT_T(KC_L)
 #define CTL_SCLN RCTL_T(KC_SCLN)
 
+// Colemak Left-hand home row mods
+#define ALT_R LALT_T(KC_R)
+#define GUI_S LGUI_T(KC_S)
+#define SFT_TT LSFT_T(KC_T) // for some reason SFT_T is not a valid name
+
+// Colemak Right-hand home row mods
+#define SFT_N RSFT_T(KC_N)
+#define GUI_E RGUI_T(KC_E)
+#define ALT_I LALT_T(KC_I)
+#define CTL_O RCTL_T(KC_O)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Alphas
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  NOOP  |   1  |   2  |   3  |   4  |   5  | NOOP |           | NOOP |   6  |   7  |   8  |   9  |   0  |  NOOP  |
+ * |  NOOP  |   1  |   2  |   3  |   4  |   5  | Lock |           | Clmk |   6  |   7  |   8  |   9  |   0  |  NOOP  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | MehTab |   Q  |   W  |   E  |   R  |   T  |  [   |           |  ]   |   Y  |   U  |   I  |   O  |   P  |   '    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -51,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                               KC_PGUP,
                                         MO(1), MEH_T(KC_TAB), KC_HOME,
   // right hand
-  KC_NO,       KC_6,   KC_7,          KC_8,   KC_9,   KC_0,     KC_NO,
+  TG(COLEMAK), KC_6,   KC_7,          KC_8,   KC_9,   KC_0,     KC_NO,
   KC_RBRC,     KC_Y,   KC_U,          KC_I,   KC_O,   KC_P,     KC_QUOT,
                KC_H,   SFT_J,         GUI_K,  ALT_L,  CTL_SCLN, KC_ENT,
   KC_EQL,      KC_N,   KC_M,          KC_COMM,KC_DOT, KC_SLSH,  KC_RSPC,
@@ -60,6 +72,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_MPLY, G(S(C(KC_4))),
   KC_PGDN,
   KC_END,  MT(MOD_LGUI, KC_ENT),KC_SPC
+),
+
+/* Keymap 1b: Colemak alphas
+ */
+
+[COLEMAK] = LAYOUT_ergodox(
+  // left hand
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_TRNS,
+  KC_TRNS, CTL_A,   ALT_R,   GUI_S,   SFT_TT,  KC_G,
+  KC_TRNS, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_TRNS,
+
+                                               KC_TRNS, KC_TRNS,
+                                                        KC_TRNS,
+                                      KC_TRNS, KC_TRNS, KC_TRNS,
+  // right hand
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_TRNS,
+           KC_M,    SFT_N,   GUI_E,   ALT_I,   CTL_O,   KC_TRNS,
+  KC_TRNS, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_TRNS,
+                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+
+  KC_TRNS, KC_TRNS,
+  KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
 /* Keymap 1: Symbol Layer
@@ -111,9 +149,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |   !  |   @  |   #  |   $  |   %  |      |           |      |   ^  |   &  |   *  |   (  |   )  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |------|           |------|      |      |      |      |      |        |
+ * |        |      |      |      |      |   [  |------|           |------|   ]  |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |      |      |      |   -  |      |           |      |   =  |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
@@ -128,8 +166,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = LAYOUT_ergodox(
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC,
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                       KC_TRNS, KC_TRNS,
                                               KC_TRNS,
@@ -137,8 +175,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // right hand
   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_TRNS, KC_TRNS,
-            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+            KC_RBRC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  KC_TRNS,  KC_EQL,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS,
   KC_TRNS,
