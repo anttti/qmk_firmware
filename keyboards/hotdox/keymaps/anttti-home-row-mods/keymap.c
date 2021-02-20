fc76e2f7d1499f5a75a5858da122c40b1f1fc762
Antti Mattila <me@rarelyneeded.com>
2021-02-20 19:47:01 +0200

    Explicit MO(1) on Colemak layer

--- keyboards/hotdox/keymaps/anttti-home-row-mods/keymap.c
+++ keyboards/hotdox/keymaps/anttti-home-row-mods/keymap.c
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
                                                KC_TRNS, KC_TRNS,
                                                         KC_TRNS,
-                                      KC_TRNS, KC_TRNS, KC_TRNS,
+                                      MO(1),   KC_TRNS, KC_TRNS,
   // right hand
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_TRNS,
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|  Del | Bcks |
+ *                                 | Mo(1)|      |------|       |------|  Del | Bcks |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
